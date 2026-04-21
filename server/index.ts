c3ecf7dbc537af98cff9747adc21f494e6dafae8
o2lv0 <fysla7932@gmail.com>
2026-04-21 17:32:06 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
-import express, { type Request, Response, NextFunction } from "express";
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes.ts";
 import { setupVite, serveStatic, log } from "./vite.ts";
 
